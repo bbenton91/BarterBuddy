7710e987a57f330e30fa0bc9790cf936c0a28194
Brad <bbenton91@gmail.com>
2021-03-05 18:13:15 -0500

    Changed icon to filter by trader name and text/link opens in new tab

--- src/Trader.svelte
+++ src/Trader.svelte
@@ -5,16 +5,23 @@
 
     export let trader:Trader;
     export let gamepediaUrl:string;
+
+    function setSearch(text:string){
+        let input = document.querySelector("#itemSearchInput") as HTMLInputElement;
+        input.value = text;
+        input.dispatchEvent(new Event("input"));
+    }
 </script>
 
 
 <div class="flex flex-col justify-items-center items-center">
-    <a href={gamepediaUrl+trader.relativeHref}>
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <a class="cursor-pointer" on:click={() => setSearch(trader.name.split(" ")[0])}>
         {#if trader.iconHref != ""}
             <!-- <Image src={"/images"+trader.iconHref} alt={trader.name} width=100 height=100 /> -->
             <img class="self-center max-w-4 lg:max-w-md" src={"/images"+trader.iconHref} alt={trader.name}>
         {/if}
     </a>
     
-    {trader.name}
+    <span class="cursor-pointer" on:click={() => window.open(gamepediaUrl+trader.relativeHref, "_blank")}>{trader.name}</span>
 </div>
