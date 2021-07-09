5b8b2f7c0de678aafddc3dcd7c61c5a010185458
Brad <bbenton91@gmail.com>
2021-07-09 14:30:50 -0400

    Added another store for the extra data

--- src/stores.ts
+++ src/stores.ts
@@ -1,6 +1,7 @@
 import { writable } from 'svelte/store';
-import type { Trade } from './Types';
+import type { ItemInfo, Trade } from './Types';
 
-export const listings = writable<Array<Trade>>( [] );
+export const listings = writable<Array<Trade>>([]);
+export const itemInfo = writable<Map<string, ItemInfo>>(new Map());
 export const filteredListings = writable<Array<Trade>>([]);
 export const showingAmount = writable<number>(25)
