1f152d4969d8e4b7df8cb1efc6894d343522c704
Brad <bbenton91@gmail.com>
2021-07-09 14:31:00 -0400

    Added a new ItemInfo type

--- src/Types.ts
+++ src/Types.ts
@@ -1,5 +1,6 @@
 export type Item = {
   name: string
+  title: string
   amount: number
   iconHref: string
   relativeHref: string
@@ -15,4 +16,28 @@ export type Trade = {
   inputs: Array<Item>
   trader: Trader
   output: Item
+}
+
+export type ItemInfo = {
+  name: string
+  quests: Array<string>
+  abbreviation: string
+  locations: Array<string>
+  hideoutReqs: Array<string>
+}
+
+export function emptyItem(): Item {
+  return { name: "", title: "", amount: -1, iconHref: "", relativeHref: "" };
+}
+
+export function emptyTrader(): Trader{
+  return { name: "", iconHref: "", relativeHref: "" };
+}
+
+export function emptyItemInfo(): ItemInfo {
+  return { name: "", quests: [], abbreviation: "", locations: [], hideoutReqs: [] };
+}
+
+export function emptyTrade(): Trade {
+  return { inputs: [], trader: emptyTrader(), output: emptyItem()};
 }
