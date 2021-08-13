d3774b6a9f9080998c70a6a2048c56565edc8392
Brad <bbenton91@gmail.com>
2021-08-13 16:12:48 -0400

    Added writables to help with tooltip states

--- src/stores.ts
+++ src/stores.ts
@@ -1,7 +1,10 @@
 import { writable } from 'svelte/store';
-import type { ItemInfo, Trade } from './Types';
+import { emptyItem, emptyItemInfo, Item, ItemInfo, Trade } from './Types';
 
 export const listings = writable<Array<Trade>>([]);
 export const itemInfo = writable<Map<string, ItemInfo>>(new Map());
 export const filteredListings = writable<Array<Trade>>([]);
 export const showingAmount = writable<number>(25)
+
+export const currentItemInfo = writable<ItemInfo>(emptyItemInfo());
+export const currentItem = writable<Item>(emptyItem());
