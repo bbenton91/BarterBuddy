b33ed2d0dd469ee8429a89f3b8ef328fc77a4b9a
Brad <bbenton91@gmail.com>
2023-08-16 20:41:25 -0400

    updates

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,6 @@
 	import Tailwindcss from './Tailwindcss.svelte';
 	import Trades from './Trades.svelte';
 	import type { ItemInfo, Trade } from './Types';
-	import * as Env from "../Env"
  // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
 	// dotenv.config()
