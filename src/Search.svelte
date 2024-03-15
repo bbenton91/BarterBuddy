6f8b0d7f75cc8b827110c6e87f2a538d9d21ce4c
Brad Benton <bradleybenton.dev@gmail.com>
2024-03-15 00:01:16 -0400

    fixed the search bar

--- src/Search.svelte
+++ src/Search.svelte
@@ -1,31 +1,56 @@
 <script lang="ts">
-    import { filteredListings, itemInfo, listings, showingAmount } from './stores'; 
+    import {
+        filteredListings,
+        itemInfo,
+        listings,
+        showingAmount,
+    } from "./stores";
 
     var timeoutFunc: NodeJS.Timeout;
     var searchText = "";
 
-    function filter(text:string){
+    function filter(text: string) {
         clearTimeout(timeoutFunc);
         timeoutFunc = setTimeout(() => {
-            var re = new RegExp(text, 'gmi');
-            // console.log(text);
-            // console.log($listings);
-            $filteredListings = $listings.filter(x => x.inputs.some(y => y.name.match(re) || $itemInfo.get(y.name)?.abbreviation.match(re)) || x.output.name.match(re) || x.trader.name.match(re) || $itemInfo.get(x.output.name)?.abbreviation.match(re));
+            var re = new RegExp(text, "gmi");
+            $filteredListings = $listings.filter(
+                (x) =>
+                    x.inputs.some(
+                        (y) =>
+                            y.name.match(re) ||
+                            $itemInfo.get(y.name)?.itemAbbreviation.match(re),
+                    ) ||
+                    x.output.name.match(re) ||
+                    x.trader.name.match(re) ||
+                    $itemInfo.get(x.output.name)?.itemAbbreviation.match(re),
+            );
             $showingAmount = text !== "" ? $filteredListings.length : 25;
         }, 500);
     }
-
 </script>
-
-<style>
-    .remove-focus:focus{
-        outline: 0;
-    }
-</style>
 
 <main>
     <div class="flex mt-5 mb-5 justify-center">
-        <input id="itemSearchInput" class="text-gray-400 bg-gray-800 rounded pl-1 pr-1 pt-1 pb-1 w-full lg:w-80" type="text" bind:value={searchText} on:input={() => filter(searchText)} placeholder="Search Here">
-        <button class="flex text-svelte text-center pl-2 pr-2 text-xl remove-focus" on:click={() => {searchText=""; filter("")}}>x</button>
+        <input
+            id="itemSearchInput"
+            class="text-gray-400 bg-gray-800 rounded pl-1 pr-1 pt-1 pb-1 w-full lg:w-80"
+            type="text"
+            bind:value={searchText}
+            on:input={() => filter(searchText)}
+            placeholder="Search Here"
+        />
+        <button
+            class="flex text-svelte text-center pl-2 pr-2 text-xl remove-focus"
+            on:click={() => {
+                searchText = "";
+                filter("");
+            }}>x</button
+        >
     </div>
 </main>
+
+<style>
+    .remove-focus:focus {
+        outline: 0;
+    }
+</style>
